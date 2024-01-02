92bf9bb785f7360fd3b170e05a5d9cc587bd82db
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-02 19:17:03 +0100

    git add pages/_app.tsx

---
+++ pages/projects.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const projects = () => {
+  return (
+    <section>
+        <h1>Project</h1>
+    </section>
+  )
+}
+
+export default projects
