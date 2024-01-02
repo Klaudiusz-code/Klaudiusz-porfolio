5b21f8a245b0baff01fd19fb78f69d72bd920725
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-02 19:09:47 +0100

    Update Button component styles and link

--- components/Button.tsx
+++ components/Button.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 const Button = () => {
   return (
-    <button className='bg-customColor text-white max-w-[120px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-md transitio hover:text-customColor hover:bg-zinc-100'>
-      <Link href='https://drive.google.com/file/d/1vMAvH0h7gqUht4VmI6LbULjYNBzGehtx/view' passHref>
+    <button className='bg-customColor mt-6 text-white max-w-[120px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-md transitio hover:text-customColor hover:bg-zinc-100'>
+      <Link href='/' passHref>
         <span className='text-[12px] xl:text-[18px]'>
-          Download CV
+          View Project
         </span>
       </Link>
     </button>
