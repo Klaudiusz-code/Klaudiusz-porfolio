1c4ad8fe2f8633883284924b7daa841da616cad4
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-02 19:10:41 +0100

    Update Navbar component with new design and transitions

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,40 +1,61 @@
-import React from 'react'
-import MenuLinks from '@/data'
-import { useRouter } from 'next/router';
+"use client"
+import React, { useEffect, useState } from 'react';
+import MenuLinks from '@/data';
+import { usePathname } from 'next/navigation';
 import { IconBase } from 'react-icons';
 import Link from 'next/link';
 
 const Navbar = () => {
+  const [isRouting, setisRouting] = useState(false);
+  const path = usePathname();
+  const [prevPath, setPrevPath] = useState<string | null>('/');
 
-  // const router = useRouter();
-  // const pathname = router.pathname;
+  useEffect(() => {
+    if (prevPath !== path) {
+      setisRouting(true);
+    }
+  }, [path, prevPath]);
 
-  return (      
-    <nav className='max-w-[1400px] mx-auto flex justify-between items-center pt-4'>
-        <img src='./Logo.png' alt="logo"
-        width={65}
-        height={65}
-        />
+  useEffect(() => {
+    if (isRouting) {
+      setPrevPath(path);
+      const timeout = setTimeout(() => {
+        setisRouting(false);
+      }, 1200);
 
-      <div className='flex gap-12 items-center'>
-        
-      {MenuLinks.map((link, index) => (
-          <div key={index} className="group relative">
+      return () => clearTimeout(timeout);
+    }
+  }, [isRouting]);
+
+  return (
+    <nav className='container mx-auto flex items-center justify-between pt-6 pb-9'>
+      <h1 className='text-2xl text-white'>
+        Klaud<span className='text-customColor text-2xl'>iusz</span>.
+      </h1>
+      <div className='flex justify-center items-center border-2 border-customColor w-[150px] h-10 rounded-sm'>
+        <span className='text-gColor font-medium text-[0.7rem]'>
+          adamaszek404gmail.com
+        </span>
+      </div>
+      <div className='flex gap-y-10 xl:flex-col items-center xl:justify-center justify-between w-full absolute bottom-0 md:right-0 lg:right-0 rounded-none lg:w-12 xl:w-12 md:px-40 xl:px-0 h-[80px] xl:h-max xl:right-4 xl:top-1/4 py-8 lg:rounded-2xl bg-navcolor'>
+        {isRouting}
+        {MenuLinks.map((link, index) => (
+          <div key={index} className='group relative'>
             <Link href={link.link}>
-              <div className='text-3xl text-customColor hover:text-white transition-all group-hover:opacity-100'>
+              <div className={`text-3xl text-white hover:text-customColor transition-all group-hover:opacity-100 ${path === link.link ? 'text-red-500' : ''}`}>  
                 {link.icon}
               </div>
             </Link>
-            <div className='absolute pr-6 hidden xl:group-hover:flex'>
+            <div className='absolute pr-4 right-0 hidden xl:group-hover:flex'>
               <div className='bg-zinc-100 transition-all mt-2 realtive flex items-center p-[8px] rounded-sm'>
-                <div className="text-[15px] font-medium ">{link.name}</div>
+                <div className='text-[15px] font-medium'>{link.name}</div>
               </div>
             </div>
           </div>
         ))}
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
