c663794db3e7d7fab21bbaa24aad1cb165bcaea3
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-03 18:30:59 +0100

    Update Navbar styling and email text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-import MenuLinks from '@/data';
+import {MenuLinks} from '@/data';
 import { usePathname } from 'next/navigation';
 import { IconBase } from 'react-icons';
 import Link from 'next/link';
@@ -33,20 +33,20 @@ const Navbar = () => {
         Klaud<span className='text-customColor text-2xl'>iusz</span>.
       </h1>
       <div className='flex justify-center items-center border-2 border-customColor w-[150px] h-10 rounded-sm'>
-        <span className='text-gColor font-medium text-[0.7rem]'>
+        <span className='text-zinc-300 font-extralight text-[0.7rem] tracking-wide'>
           adamaszek404gmail.com
         </span>
       </div>
-      <div className='flex gap-y-10 xl:flex-col items-center xl:justify-center justify-between w-full absolute bottom-0 md:right-0 lg:right-0 rounded-none lg:w-12 xl:w-12 md:px-40 xl:px-0 h-[80px] xl:h-max xl:right-4 xl:top-1/4 py-8 lg:rounded-2xl bg-navcolor'>
+      <div className='flex gap-y-5 xl:flex-col items-center xl:justify-center justify-between w-full bg-customColor lg:bg-transparent absolute bottom-0 md:right-0 lg:right-0 rounded-none lg:w-12 xl:w-12 md:px-40 xl:px-0 h-[80px] xl:h-max xl:right-14 xl:top-1/4 py-8 lg:rounded-2xl'>
         {isRouting}
         {MenuLinks.map((link, index) => (
           <div key={index} className='group relative'>
             <Link href={link.link}>
-              <div className={`text-3xl text-white hover:text-customColor transition-all group-hover:opacity-100 ${path === link.link ? 'text-red-500' : ''}`}>  
+              <div className={`text-[1.1rem] text-white  bg-whiteOp hover:bg-customColor w-14 h-14 flex items-center justify-center rounded-full transition-all group-hover:opacity-100 ${path === link.link ? 'bg-[#34C0C9' : ''}`}>  
                 {link.icon}
               </div>
             </Link>
-            <div className='absolute pr-4 right-0 hidden xl:group-hover:flex'>
+            <div className='absolute pr-4 right-12 bottom-2 hidden xl:group-hover:flex'>
               <div className='bg-zinc-100 transition-all mt-2 realtive flex items-center p-[8px] rounded-sm'>
                 <div className='text-[15px] font-medium'>{link.name}</div>
               </div>
