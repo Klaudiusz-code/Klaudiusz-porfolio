ba200ce4f46178e612a01154d86ad126f4a62d43
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-03 18:35:18 +0100

    Update colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,7 +13,9 @@ const config: Config = {
         customColor: '#34C0C9',
         gColor: '#818488',
         boxColor: 'rgba(34, 54, 62,0.3)',
-        hoverColor: 'rgba(40, 50, 61,0.9)'
+        hoverColor: 'rgba(40, 50, 61,0.9)',
+        navcolor: 'rgba(52, 192, 201,0.9)',
+        whiteOp: 'rgba(255, 255, 255,0.2)',
       },
       screens: {
         'xl': '1280px',
