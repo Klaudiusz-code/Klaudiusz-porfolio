64fd2e7cf68b6524a9ccca6cd3c081e4d1e1c848
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-03 18:34:31 +0100

    Update icons in MenuLinks and introduce new skills, services, and learn data

--- data.js
+++ data.js
@@ -1,10 +1,13 @@
 
 import { FaHome, FaClipboardList } from "react-icons/fa";
-import { GrServices } from "react-icons/gr";
+import { MdDesignServices } from "react-icons/md";
 import { IoMdHome } from "react-icons/io";
-import { IoIosContact } from "react-icons/io";
 import { GrProjects } from "react-icons/gr";
-
+import { HiOutlineMail } from "react-icons/hi";
+import { SlArrowRight } from "react-icons/sl";
+import { FaLaptopCode } from "react-icons/fa";
+import { FaRegCopy } from "react-icons/fa6";
+import { FaChartBar } from "react-icons/fa";
 
 const MenuLinks = [
     {
@@ -18,15 +21,93 @@ const MenuLinks = [
         name: 'Project',
     },
     {
-        icon: <GrServices />,
+        icon: <MdDesignServices />,
         link: "/services",
         name: 'Services',
     },
     {
-        icon: <IoIosContact />,
+        icon: <HiOutlineMail />,
         link: "/contact",
         name: 'Contact',
     },
 ];
 
-export default MenuLinks
+
+const MySkills = [
+    {
+        name: 'HTML',
+        icon: <SlArrowRight />
+    },
+    {
+        name: 'SCSS',
+        icon: <SlArrowRight />
+    },
+    {
+        name: 'TAILWIND',
+        icon: <SlArrowRight />
+    },
+    {
+        name: 'JavaScript',
+        icon: <SlArrowRight />
+    },
+    {
+        name: 'React',
+        icon: <SlArrowRight />
+    },
+    {
+        name: 'NodeJS',
+        icon: <SlArrowRight />
+    },
+    {
+        name: 'ExpressJS',
+        icon: <SlArrowRight />
+    },
+    {
+        name: 'Strapi',
+        icon: <SlArrowRight />
+    },
+    {
+        name: 'MongoDB',
+        icon: <SlArrowRight />
+    },
+    {
+        name: 'Figma',
+        icon: <SlArrowRight />
+    },
+]
+const Learn = [
+    
+    {
+       name: 'TypeScript',
+       icon: <SlArrowRight />
+    },
+    {
+       name: 'NextJS',
+       icon: <SlArrowRight />
+    },
+   
+]
+const MyServices = [
+    {
+        name: 'Development',
+        icon: <FaLaptopCode />,
+        desc:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio sed officiis possimus sit adipisci,',
+    },
+    {
+        name: 'SEO',
+        icon: <FaChartBar />,
+        desc:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio sed officiis possimus sit adipisci,',
+    },
+    {
+        name: 'Copy Write',
+        icon: <FaRegCopy />,
+        desc:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio sed officiis possimus sit adipisci,',
+    },
+]
+
+export {
+    MenuLinks,
+    MySkills,
+    MyServices,
+    Learn
+};
