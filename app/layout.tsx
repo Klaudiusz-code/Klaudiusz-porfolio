8ec70d3418986cdeb33cf101a49a527a93e2005a
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-07 19:27:23 +0100

    fix: correct import paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,17 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { ReactNode } from 'react';
-import './globals.css';
-import Navbar from '@/components/Navbar';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 const metadata: Metadata = {
-  title: 'Klaudiusz Adamaszek',
-  description: 'Portfolio',
+  title: "Klaudiusz Adamaszek",
+  description: "Portfolio",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Navbar />
@@ -23,4 +19,3 @@ export default function RootLayout({
     </>
   );
 }
-
