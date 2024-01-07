b7045c3861a38430df085504485146b409981fde
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-07 19:36:46 +0100

    feat: integrate AnimatePresence and Transition in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,21 @@
+// pages/_app.tsx
 import { AppProps } from 'next/app';
 import RootLayout from '@/app/layout';
+import Transition from '@/components/Transition';
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
   return (
     <RootLayout>
-      <Component {...pageProps} />
+      <AnimatePresence mode='wait'>
+        <motion.div key={router.route} initial='initial' animate='animate' exit='exit'>
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </RootLayout>
   );
 }
