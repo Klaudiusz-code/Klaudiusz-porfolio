bc31fb6d34cea2a85b19eaf754ba742f54d98d04
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-07 19:30:25 +0100

    refactor: update Button component link

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,14 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const Button = () => {
   return (
-    <button className='bg-customColor mt-6 text-white max-w-[120px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-md transitio hover:text-customColor hover:bg-zinc-100'>
-      <Link href='/' passHref>
-        <span className='text-[12px] xl:text-[18px]'>
-          View Project
-        </span>
+    <button className="bg-customColor mt-6 text-white max-w-[120px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-md transitio hover:text-customColor hover:bg-zinc-100">
+      <Link href="/projects" passHref>
+        <span className="text-[12px] xl:text-[18px]">View Project</span>
       </Link>
     </button>
-    
-  )
-}
+  );
+};
 
 export default Button;
