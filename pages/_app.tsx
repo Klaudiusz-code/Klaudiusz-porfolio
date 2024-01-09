417570695c2adc597f6ecfc1d10e2bf03d705bcd
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-09 19:40:13 +0100

    Update: handlePageLoading function in app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,41 @@
-// pages/_app.tsx
-import { AppProps } from 'next/app';
-import RootLayout from '@/app/layout';
-import Transition from '@/components/Transition';
-import { useRouter } from 'next/router';
-import { AnimatePresence, motion } from 'framer-motion';
+import { AppProps } from "next/app";
+import RootLayout from "@/app/layout";
+import Transition from "@/components/Transition";
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import Loader from "@/components/Loader";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const handlePageLoading = () => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    };
+
+    handlePageLoading();
+  }, []);
 
   return (
-    <RootLayout>
-      <AnimatePresence mode='wait'>
-        <motion.div key={router.route} initial='initial' animate='animate' exit='exit'>
-          <Transition />
-          <Component {...pageProps} />
-        </motion.div>
+    <RootLayout hideNavbar={isLoading}>
+      <AnimatePresence mode="wait">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <motion.div
+            key={router.route}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+          >
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        )}
       </AnimatePresence>
     </RootLayout>
   );
