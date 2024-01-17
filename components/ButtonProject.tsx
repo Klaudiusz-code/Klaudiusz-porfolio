64a524fa63129d747725871156ac8f318201cfec
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-17 18:51:48 +0100

    Update Button component styling

--- components/ButtonProject.tsx
+++ components/ButtonProject.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Button = () => {
   return (
-    <button className="bg-customColor mt-6 max-w-[120px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-md transitio hover:text-customColor hover:bg-navcolor transition-all duration-150">
+    <button className="border border-customColor mt-6 max-w-[120px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-md transitio hover:text-customColor hover:bg-navcolor transition-all duration-150">
       <Link href="/projects" passHref>
         <span className="text-[14px] text-white xl:text-[16px] font-bold">
           Projekty
