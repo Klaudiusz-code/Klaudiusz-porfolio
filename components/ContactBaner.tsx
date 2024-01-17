106f183500c1a00e6ae00770f4970f54963c714b
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-17 18:52:49 +0100

    Update ContactBanner component styling

--- components/ContactBaner.tsx
+++ components/ContactBaner.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
-import ButtonContact from './ButtonContact'
+import React from "react";
+import ButtonContact from "./ButtonContact";
 
 const ContactBaner = () => {
   return (
-    <div className="h-[300px] flex items-center justify-center bg-cover mt-32 mb-12 bg-center bg-contactBanner rounded-lg">
-    <div className="text-center text-white">
-      <h2 className="text-4xl font-bold mb-4">POROZMAWIAJMY O TWOIM <span className='text-customColor'>PROJEKCIE</span></h2>
-      <p className="text-lg mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <ButtonContact/>
+    <div className="h-[300px] border border-customColor flex items-center justify-center bg-cover mt-32 mb-12 bg-center bg-contactBanner rounded-lg">
+      <div className="text-center text-white">
+        <h2 className="text-4xl font-bold mb-4 font-sans">
+          POROZMAWIAJMY O TWOIM{" "}
+          <span className="text-customColor">PROJEKCIE</span>
+        </h2>
+        <p className="text-lg mb-8">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <ButtonContact />
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default ContactBaner
+export default ContactBaner;
