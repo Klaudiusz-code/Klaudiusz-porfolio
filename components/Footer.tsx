8c07386d0420fba200e7508f79fa8a90b7207ad2
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-17 19:02:07 +0100

    feat: Add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full bg-[#0a1b20] py-4'>
+        <div className="container mx-auto text-center">
+            <span className='text-white font-sans'>
+            © Klaudiusz Adamaszek | Strony Internetowe 2024
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
