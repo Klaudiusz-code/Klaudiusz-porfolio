da0a85b231514582024f4d137cc82ed0ea5013db
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-21 18:32:33 +0100

    feat: Add logo to navbar components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,24 +5,26 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import ButtonContact from "./ButtonContact";
+import Image from "next/image";
+import Logo from "@/public/logo2.png";
 
 const Navbar = () => {
   const router = useRouter();
   const pathname = router.pathname;
   return (
     <nav className="container mx-auto flex items-center justify-between pt-6 pb-9 relative z-10">
-      <h1 className="text-2xl text-white">
+      <div className="flex flex-col items-center justify-center text-2xl text-white">
         <a href="/">
-          Klaud<span className="text-customColor text-2xl">iusz</span>.
+          <Image src={Logo} width={110} height={110} alt="Logo"></Image>
         </a>
-      </h1>
+      </div>
       <ButtonContact />
       <div className="flex gap-y-5 xl:flex-col items-center xl:justify-center justify-between w-full bg-customColor lg:bg-transparent fixed bottom-0 md:right-0 lg:right-0 rounded-none lg:w-12 xl:w-12 md:px-40 xl:px-0 h-[80px] xl:h-max xl:right-14 xl:top-1/4 py-8 lg:rounded-2xl">
         {MenuLinks.map((link, index) => {
           return (
             <Link
               className={`${
-                link.link === pathname && "bg-teal-400"
+                link.link === pathname && "bg-teal-500"
               } text-[1.1rem] text-white  bg-whiteOp hover:bg-customColor w-14 h-14 flex items-center justify-center rounded-full transition-all group-hover:opacity-1000`}
               href={link.link}
               key={index}
