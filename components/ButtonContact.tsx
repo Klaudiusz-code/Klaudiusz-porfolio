47f09e31af69f6354601e13f41ed38a9b7e108cf
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-21 18:17:52 +0100

    style: update style for ButtonContact

--- components/ButtonContact.tsx
+++ components/ButtonContact.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const ButtonContact = () => {
   return (
-    <button className="border border-customColor mt-6 max-w-[120px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-md transitio hover:text-customColor hover:bg-navcolor transition-all duration-150">
+    <button className="bg-customColor max-w-[180px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-sm hover:text-customColor hover:bg-navcolor transition-all duration-150">
       <Link href="/contact" passHref>
       <span className="text-[14px] text-white xl:text-[16px] font-bold">
           Skontaktuj się!
