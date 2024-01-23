7f177e895ba11d8dc80a3b49f17230e26d75d207
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-23 18:04:05 +0100

    style: Improve AccordionUi appearance and responsiveness

--- components/Acordin/AcordinUi.tsx
+++ components/Acordin/AcordinUi.tsx
@@ -9,9 +9,11 @@ const AccordionUi = ({ title, answer }) => {
     <div className="py-3 font-sans">
       <button
         onClick={() => setAccordionOpen(!accordionOpen)}
-        className="flex justify-between w-full rounded-md border border-gray-700  transition-all duration-150 p-4"
+        className="flex justify-between w-full items-center rounded-md border border-gray-700  transition-all duration-300 p-4 hover:border-customColor "
       >
-        <span className="text-gColor text-[1.1rem]">{title}</span>
+        <span className="text-gColor text-[0.9rem] lg:text-[1rem]">
+          {title}
+        </span>
         {accordionOpen ? (
           <HiX className="text-gColor shrink-0 ml-8 text-[1.1rem]" size={20} />
         ) : (
@@ -19,7 +21,7 @@ const AccordionUi = ({ title, answer }) => {
         )}
       </button>
       <div
-        className={`grid overflow-hidden mt-1 transition-all duration-300 ease-in-out text-[#0077cc] font-medium text-sm ${
+        className={`grid overflow-hidden mt-1 transition-all duration-300 ease-in-out text-red-400  text-[0.8rem] lg:text-[1rem] text-center md:text-left ${
           accordionOpen
             ? "grid-rows-[1fr] opacity-100"
             : "grid-rows-[0fr] opacity-0"
