8512d51844340bf7476ca9346d613dcdd058a589
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-23 18:06:35 +0100

    style: Adjust styling in ButtonContact component

--- components/ButtonContact.tsx
+++ components/ButtonContact.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const ButtonContact = () => {
   return (
-    <button className="bg-customColor max-w-[180px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-sm hover:text-customColor hover:bg-navcolor transition-all duration-150">
+    <button className="bg-customColor max-w-[200px]  p-2 xl:min-w-[150px] h-12 rounded-sm hover:text-customColor hover:bg-navcolor transition-all duration-150">
       <Link href="/contact" passHref>
-      <span className="text-[14px] text-white xl:text-[16px] font-bold">
+        <span className="text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] text-white xl:text-[16px] font-bold">
           Skontaktuj się!
         </span>
       </Link>
