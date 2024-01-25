b2d58c16abfab5729556a695615a5bbe2f92639f
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-25 18:30:16 +0100

    Update ButtonContact component to accept dynamic buttonText prop

--- components/ButtonContact.tsx
+++ components/ButtonContact.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Link from "next/link";
 
-const ButtonContact = () => {
+const ButtonContact = ({buttonText}) => {
   return (
     <button className="bg-customColor max-w-[200px]  p-2 xl:min-w-[150px] h-12 rounded-sm hover:text-customColor hover:bg-navcolor transition-all duration-150">
       <Link href="/contact" passHref>
         <span className="text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] text-white xl:text-[16px] font-bold">
-          Skontaktuj się!
+          {buttonText}
         </span>
       </Link>
     </button>
