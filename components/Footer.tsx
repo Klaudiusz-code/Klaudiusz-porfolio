a0877a4085af6757e202df93548be4d365974475
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-28 16:24:28 +0100

    feat: Change footer text from 'Strony Internetowe' to 'Web Development'

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className="w-full bg-[#0a1b20] py-4">
       <div className="container mx-auto text-center">
         <span className="text-white font-sans">
-          © Klaudiusz Adamaszek | Strony Internetowe 2024
+          © Klaudiusz Adamaszek | Web Development 2024
         </span>
       </div>
     </div>
