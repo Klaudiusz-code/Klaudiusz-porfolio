3b17502993ec0c60c226c3f3bf8eefdf96e4f646
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-29 17:16:51 +0100

    style: Update button styling in ButtonContact component

--- components/ButtonProject.tsx
+++ components/ButtonProject.tsx
@@ -4,7 +4,7 @@ import { FaArrowRight } from "react-icons/fa6";
 
 const Button = () => {
   return (
-    <button className="border  border-customColor mt-8 max-w-[120px] md:max-w-[130px] p-2 xl:min-w-[150px] h-12 rounded-md transitio hover:text-customColor hover:bg-navcolor transition-all duration-150">
+    <button className="border  border-customColor mt-8 max-w-[120px] md:max-w-[130px] py-2 px-6  xl:min-w-[150px] h-12 rounded-md transitio hover:text-customColor hover:bg-navcolor transition-all duration-150">
       <Link href="/projects" passHref>
         <div className="flex items-center justify-center text-[14px] text-white xl:text-[16px] font-bold">
           Projekty
