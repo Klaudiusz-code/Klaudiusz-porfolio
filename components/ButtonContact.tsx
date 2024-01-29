380bc5e9d0b6b3b82a139bca5d535281ea48472d
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-29 17:16:11 +0100

    style: Update button styling in ButtonContact component

--- components/ButtonContact.tsx
+++ components/ButtonContact.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const ButtonContact = ({buttonText}) => {
   return (
-    <button className="bg-customColor max-w-[200px]  p-2 xl:min-w-[150px] h-12 rounded-sm hover:text-customColor hover:bg-navcolor transition-all duration-150">
+    <button className="max-w-[200px] border border-customColor  p-2 xl:min-w-[150px] h-12 rounded-lg hover:text-customColor hover:bg-navcolor transition-all duration-150">
       <Link href="/contact" passHref>
         <span className="text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] text-white xl:text-[16px] font-bold">
           {buttonText}
