f2c6e3fe1c08211e72abb83fb57da1a1c15b9c1b
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-30 16:44:06 +0100

    refactor: Update contact button in ContactBanner component

--- components/ContactBaner.tsx
+++ components/ContactBaner.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ButtonContact from "./ButtonContact";
+import CustomButton from "./CustomButton";
 
 const ContactBaner = () => {
   return (
-    <div className="h-[300px] flex items-center justify-center bg-cover mt-32 mb-12 bg-center bg-contactBanner rounded-lg">
+    <div className="min-h-[400px] flex items-center justify-center bg-cover mt-32 mb-12 bg-center bg-contactBanner rounded-lg">
       <div className="text-center text-white">
-        <h2 className="text-[1.4rem] md:text-[1.5rem] lg:text-[1.7rem] font-bold mb-4 font-sans">
+        <h2 className="text-[1.4rem] md:text-[1.5rem] lg:text-[2.2rem] font-bold mb-4 font-sans">
           POROZMAWIAJMY O TWOIM{" "}
           <span className="text-customColor">PROJEKCIE</span>
         </h2>
         <p className="text-[0.9rem] md:text-[1rem] lg:text-[1.1rem] mb-8">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
-        <ButtonContact buttonText="Napisz do mnie!" />
+        <CustomButton text="Napisz do mnie!" link="/contact" />
       </div>
     </div>
   );
