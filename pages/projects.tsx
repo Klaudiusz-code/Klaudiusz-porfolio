b6ba744b563722e25695bc3180ce2a83709f59d3
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-30 16:47:59 +0100

    refactor: Replace ProjectQuoteButton with CustomButton in Projects component

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,7 +4,7 @@ import { FaEye, FaGithub } from "react-icons/fa";
 import Head from "next/head";
 import { ProjectsData } from "@/data";
 import ButtonContact from "@/components/ButtonContact";
-import ProjectQuoteButton from "@/components/ProjectQuoteButton";
+import CustomButton from "@/components/CustomButton";
 
 const Projects = () => {
   return (
@@ -22,7 +22,7 @@ const Projects = () => {
             <h1 className="text-[1.6rem] lg:text-[2.6rem] font-[600] tracking-wide text-[#fff]">
               Zrealizowane projekty
             </h1>
-            <p className="text-[#e7e7e7] leading-8 mt-3 mb-6 font-sans font-[400] text-[1rem] md:text-[1.2rem] lg:text-[1.4rem] w-full md:max-w-[80%] lg:max-w-[70%] tracking-wide">
+            <p className="text-[#e7e7e7] leading-8 mt-1 mb-6 font-sans font-[400] text-[1rem] md:text-[1.2rem] lg:text-[1.4rem] w-full md:max-w-[80%] lg:max-w-[70%] tracking-wide">
               Jestem w stanie stworzyć dla ciebie unikalną i funkcjonalną{" "}
               <span className="underline underline-offset-2 hover:text-hoverColor">
                 Strone,{" "}
@@ -36,7 +36,7 @@ const Projects = () => {
               projekt marzeń!
             </p>
             <div className="mt-6 text-center md:text-left ">
-              <ProjectQuoteButton />
+              <CustomButton text="Darmowa Wycena Projektu" link="/contact" />
             </div>
           </div>
           <div className="w-full mx-auto mt-14">
@@ -47,7 +47,7 @@ const Projects = () => {
               {ProjectsData.map((project, index) => (
                 <div className="flex flex-row mb-4">
                   <div className="border border-gray-700 rounded-sm w-full min-h-[350px] flex items-center justify-center">
-                    <div className="h-[10rem] w-[10rem] relative"> 
+                    <div className="h-[10rem] w-[10rem] relative">
                       <div className="absolute h-[0.03rem] w-full bg-gray-700 transform rotate-45 origin-center"></div>
                       <div className="absolute h-[0.03rem] w-full bg-gray-700 transform -rotate-45 origin-center"></div>
                     </div>
