db51b2c0424d807888f31ce32232a4da37bfaa0d
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-31 13:47:17 +0100

    style: Fine-tune styling and typography in ButtonContact component

--- components/ButtonContact.tsx
+++ components/ButtonContact.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 const ButtonContact = ({buttonText}) => {
   return (
-    <button className="max-w-[200px] border border-customColor  p-2 xl:min-w-[150px] h-12 rounded-lg hover:text-customColor hover:bg-navcolor transition-all duration-150">
+    <button className="max-w-[200px] border border-customColor  py-2 px-5 xl:min-w-[150px] h-12 rounded-lg hover:text-customColor hover:bg-navcolor transition-all duration-150">
       <Link href="/contact" passHref>
-        <span className="text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] text-white xl:text-[16px] font-bold">
+        <span className="text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] text-white xl:text-[16px] font-[400]">
           {buttonText}
         </span>
       </Link>
