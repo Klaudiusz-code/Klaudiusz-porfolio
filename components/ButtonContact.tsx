e06b5a4dff6b5e25ecce352f34cf8c1e40f5f709
Klaudiusz_dev <muminowy123@gmail.com>
2024-01-31 14:00:48 +0100

    Refactor: ButtonContact to Use TypeScript

--- components/ButtonContact.tsx
+++ components/ButtonContact.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Link from "next/link";
 
-const ButtonContact = ({buttonText}) => {
+interface buttonProps {
+buttonText: string;
+}
+
+const ButtonContact: React.FC<buttonProps> = ({buttonText}) => {
   return (
     <button className="max-w-[200px] border border-customColor  py-2 px-5 xl:min-w-[150px] h-12 rounded-lg hover:text-customColor hover:bg-navcolor transition-all duration-150">
       <Link href="/contact" passHref>
