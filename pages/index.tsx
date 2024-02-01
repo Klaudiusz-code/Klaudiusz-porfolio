b7948e64e6a5609f64850852be53f1c4e0458e50
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-01 13:57:23 +0100

    refactor: Update home page UI with motion animations for enhanced visual appeal

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,21 +29,36 @@ const Home = () => {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />{" "}
       </Head>
-      <div
-        className="w-full relative container mx-auto mt-16 flex items-center justify-center text-center flex-col lg:flex-row bg-body-bg "
-      >
+      <div className="w-full relative container mx-auto mt-16 flex items-center justify-center text-center flex-col lg:flex-row bg-body-bg ">
         <div className="w-full lg:pr-8 mt-27 md:mt-28 xl:mt-32 lg:order-2">
-          <h1 className="mx-auto font-[600] text-white w-full font-sans text-[1.7rem] md:max-w-[80%] sm:text-[2rem] lg:text-[2.9rem] mb-4 tracking-wide">
+          <motion.h1
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="mx-auto font-[700] text-white w-full font-sans text-[1.7rem] md:max-w-[80%] sm:text-[2rem] lg:text-[2.9rem] mb-4 tracking-wide"
+          >
             Ciekawi Cię, jak{" "}
             <span className="underline underline-offset-4 ">skutecznie</span>{" "}
             wypromować swoją firmę czy projekt w sieci?
-          </h1>
-          <h2 className="mx-auto w-full md:max-w-[60%] mt-8 text-gColor font-medium text-[0.9rem] leading-7 lg:leading-9 md:text-[1rem] lg:text-[1.1rem] font-sans">
-            Odpowiedź jest prosta – z wykorzystaniem profesjonalnej strony
-            internetowej! Jestem pasjonatem web developmentu i chętnie pomogę Ci
-            w stworzeniu unikalnej przestrzeni online.
-          </h2>
-          <Button />
+          </motion.h1>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 1 }}
+          >
+            <h2 className="mx-auto w-full md:max-w-[60%] mt-8 text-gColor font-medium text-[0.9rem] leading-7 lg:leading-9 md:text-[1rem] lg:text-[1.1rem] font-sans">
+              Odpowiedź jest prosta – z wykorzystaniem profesjonalnej strony
+              internetowej! Jestem pasjonatem web developmentu i chętnie pomogę
+              Ci w stworzeniu unikalnej przestrzeni online.
+            </h2>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
+            >
+              <Button />
+            </motion.div>
+          </motion.div>
         </div>
       </div>
       <Aboutme />
