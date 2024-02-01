16aa42ac646902dee5b39debd6a1a36eeeca91ef
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-01 19:27:58 +0100

    Style: Adjust styles and layout in Services component

--- pages/services.tsx
+++ pages/services.tsx
@@ -43,7 +43,7 @@ const services = () => {
         </div>
       </div>
 
-      <div className="container mx-auto mt-10 mb-5  rounded-lg p-1">
+      <div className="container mx-auto mt-10  rounded-lg p-1">
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 gap-x-24 justify-center">
           {MySkills.map((item, index) => (
             <div className="flex flex-col items-center" key={index}>
