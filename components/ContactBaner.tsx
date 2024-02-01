4cfdfb94352bcf308a2d239b4f3f570a7c5e8fa9
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-01 19:25:27 +0100

    style: Update border styling in ContactBaner component

--- components/ContactBaner.tsx
+++ components/ContactBaner.tsx
@@ -4,7 +4,7 @@ import CustomButton from "./CustomButton";
 
 const ContactBaner = () => {
   return (
-    <div className="min-h-[400px] flex items-center justify-center bg-cover mt-32 mb-12 bg-center bg-contactBanner rounded-lg">
+    <div className="min-h-[400px] flex items-center justify-center bg-cover mt-32  bg-center border-spacing-6  border-[4px] border-customColor mb-2 bg-contactBanner rounded-lg">
       <div className="text-center text-white">
         <h2 className="text-[1.4rem] md:text-[1.5rem] lg:text-[2.2rem] font-bold mb-4 font-sans">
           POROZMAWIAJMY O TWOIM{" "}
