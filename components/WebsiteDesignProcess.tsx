68c075e32add7933b1aeac6ce2fa2dbd85e597ab
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-01 14:04:35 +0100

    feat: Added key for data maps

--- components/WebsiteDesignProcess.tsx
+++ components/WebsiteDesignProcess.tsx
@@ -46,7 +46,9 @@ const WebsiteDesignProcess = () => {
         <div className="w-full mx-auto mt-6">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {data.map((item, index) => (
-              <div className="flex items-center flex-col flex-wrap mx-auto mt-6 text-center">
+              <div
+              key={index}
+              className="flex items-center flex-col flex-wrap mx-auto mt-6 text-center">
                 <div className="bg-customColor h-16 w-16 flex items-center justify-center rounded-full">
                   <span className="text-white font-sans font-[600] text-[1.3rem]">
                     {item.num}
