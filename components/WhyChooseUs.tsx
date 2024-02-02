730e899862fa8c299c37624998e7f1f8b90985e2
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-02 16:03:11 +0100

    Style: Adjust font size for service section header

--- components/WhyChooseUs.tsx
+++ components/WhyChooseUs.tsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const WhyChooseUs = () => {
   return (
-    <div className="w-full mx-auto text-center mt-28 mb-12">
-      <span className="px-2 py-3 text-[0.9rem] md:text-[0.1rem] lg:text-[1.1rem] bg-gray-700 text-white font-bold rounded-md">
+    <div className="w-full mx-auto text-center mt-12 md:mt-20  lg:mt-28 mb-12">
+      <span className="px-2 py-3 text-[0.9rem] md:text-[1rem] lg:text-[1.1rem] bg-gray-700 text-white font-bold rounded-md">
         Dlaczego Warto Skorzystać z Moich Usług?
       </span>
 
-      <div className="w-full border border-customColor bg-gray-800 rounded-lg  mx-auto font-sans mt-24 lg:mt-32 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 items-center gap-y-4 md:gap-y-0 place-items-center place-content-center">
+      <div className="w-full border border-customColor bg-gray-800 rounded-lg  mx-auto font-sans mt-16 md:mt-24 lg:mt-32 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 items-center gap-y-4 md:gap-y-0 place-items-center place-content-center">
         {" "}
         {ServicesWebsite.map((service, index) => (
           <div
