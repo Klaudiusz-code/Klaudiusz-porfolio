8e59949850ead402491c076345c68f51b246ffa7
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-03 09:25:17 +0100

    refactor: Changed div to section in AccordionUi component and updated colors

--- components/Acordin/AcordinUi.tsx
+++ components/Acordin/AcordinUi.tsx
@@ -11,22 +11,22 @@ const AccordionUi: React.FC<AccordionUiProps> = ({ title, answer }) => {
   const [accordionOpen, setAccordionOpen] = useState(false);
 
   return (
-    <div className="py-3 font-sans">
+    <div className="w-full lg:max-w-[90%] mx-auto py-3 font-sans">
       <button
         onClick={() => setAccordionOpen(!accordionOpen)}
         className="flex justify-between w-full items-center rounded-md border border-gray-700  transition-all duration-300 p-4 hover:border-customColor "
       >
-        <span className="text-gColor text-[0.9rem] lg:text-[1rem]">
+        <span className="text-customColor text-[0.9rem] lg:text-[1rem]">
           {title}
         </span>
         {accordionOpen ? (
-          <HiX className="text-gColor shrink-0 ml-8 text-[1.1rem]" size={20} />
+          <HiX className="text-customColor shrink-0 ml-8 text-[1.1rem]" size={20} />
         ) : (
-          <IoIosArrowDown className="text-gColor shrink-0 ml-8" size={20} />
+          <IoIosArrowDown className="text-customColor shrink-0 ml-8" size={20} />
         )}
       </button>
       <div
-        className={`grid overflow-hidden mt-1 transition-all duration-300 ease-in-out text-red-400  text-[0.8rem] lg:text-[1rem] text-center md:text-left ${
+        className={`grid overflow-hidden mt-1 transition-all duration-300 ease-in-out text-gColor  text-[0.8rem] lg:text-[1rem] text-center md:text-left ${
           accordionOpen
             ? "grid-rows-[1fr] opacity-100"
             : "grid-rows-[0fr] opacity-0"
