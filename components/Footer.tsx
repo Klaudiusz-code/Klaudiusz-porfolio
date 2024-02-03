634e67d9d112eb88d078a4f1e6cf2326942b8b00
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-03 09:21:42 +0100

    style: Update Footer component to use semantic HTML

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="w-full bg-[#0a1b20] py-4">
+    <footer className="w-full bg-[#0a1b20] py-4">
       <div className="container mx-auto text-center">
         <span className="text-white font-sans">
           © Klaudiusz Adamaszek | Web Development 2024
         </span>
       </div>
-    </div>
+    </footer>
   );
 };
 
