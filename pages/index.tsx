d1cbfef7e2804cd005c8700e880ceb47874f7648
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-03 09:31:47 +0100

    Refactor: Adjusted classNames and animation duration in the Home component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home = () => {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />{" "}
       </Head>
-      <div className="w-full relative container mx-auto mt-16 flex items-center justify-center text-center flex-col lg:flex-row bg-body-bg ">
+      <section className="w-full relative container mx-auto mt-16 flex items-center justify-center text-center flex-col lg:flex-row bg-body-bg ">
         <div className="w-full lg:pr-8 mt-27 md:mt-28 xl:mt-32 lg:order-2">
           <motion.h1
             initial={{ opacity: 0, x: -100 }}
@@ -38,13 +38,13 @@ const Home = () => {
             className="mx-auto font-[700] text-white w-full font-sans text-[1.7rem] md:max-w-[80%] sm:text-[2rem] lg:text-[2.9rem] mb-4 tracking-wide"
           >
             Ciekawi Cię, jak{" "}
-            <span className="underline underline-offset-4 ">skutecznie</span>{" "}
+            <span className="text-customColor">skutecznie</span>{" "}
             wypromować swoją firmę czy projekt w sieci?
           </motion.h1>
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 1.2, delay: 1 }}
+            transition={{ duration: 1.2, delay: 1.2 }}
           >
             <h2 className="mx-auto max-w-[95%] md:max-w-[60%] mt-8 text-gColor font-medium text-[0.9rem] leading-7 lg:leading-9 md:text-[1rem] lg:text-[1.1rem] font-sans">
               Odpowiedź jest prosta – z wykorzystaniem profesjonalnej strony
@@ -60,7 +60,7 @@ const Home = () => {
             </motion.div>
           </motion.div>
         </div>
-      </div>
+      </section>
       <Aboutme />
       <div className="mt-16 md:mt-20 lg:mt-42 mb-28">
         <WebsiteDesignProcess />
