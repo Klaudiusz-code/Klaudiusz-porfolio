5c0d119b7e581a139568e125f5068993742f58f6
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-03 09:33:09 +0100

    style: Refactored the structure of the "services" component to use semantic HTML5 elements and added consistent padding to improve readability and maintainability

--- pages/services.tsx
+++ pages/services.tsx
@@ -10,7 +10,7 @@ import ContactBaner from "@/components/ContactBaner";
 
 const services = () => {
   return (
-    <>
+    <section className="container mx-auto px-2 font-sans">
       <Head>
         <title>
           Usługi - Strony Internetowe, Sklepy Internetowe, SEO | Lubycza
@@ -18,29 +18,28 @@ const services = () => {
           Web Developer
         </title>
       </Head>
-      <div className="container mx-auto font-sans">
-        <div className="w-full text-center mt-2">
-          <h1 className="tex-white font-sans font-[900] text-white text-[1.8rem] md:text-[2.1rem] lg:text-[2.8rem] tracking-wide">
-            OFERTA & NARZĘDZIA
-          </h1>
-          <p className="text-gColor text-[0.9rem] md:text-[1rem] lg:text-[1.3rem] font-sans font-[400]">
-            Co mogę dla ciebie zrobić!
-          </p>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 mt-8 justify-center">
-          {MyServices.map((service, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-center text-center text-white border border-gray-700 rounded-md min-h-[183px] transition-all duration-100 even:bg-customColor"
-            >
-              <span className="text-[2.4rem] pt-3">{service.icon}</span>
-              <h1 className="mt-2 text-[26px] font-bold">{service.name}</h1>
-              <p className="max-w-[80%] mt-1 text-white font-light text-[0.9rem]">
-                {service.desc}
-              </p>
-            </div>
-          ))}
-        </div>
+
+      <div className="w-full text-center mt-2">
+        <h1 className="tex-white font-sans font-[900] text-white text-[1.8rem] md:text-[2.1rem] lg:text-[2.8rem] tracking-wide">
+          OFERTA & NARZĘDZIA
+        </h1>
+        <p className="text-gColor text-[0.9rem] md:text-[1rem] lg:text-[1.3rem] font-sans font-[400]">
+          Co mogę dla ciebie zrobić!
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 mt-8 justify-center">
+        {MyServices.map((service, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center text-center text-white border border-gray-700 rounded-md min-h-[183px] transition-all duration-100 even:bg-customColor"
+          >
+            <span className="text-[2.4rem] pt-3">{service.icon}</span>
+            <h1 className="mt-2 text-[26px] font-bold">{service.name}</h1>
+            <p className="max-w-[80%] mt-1 text-white font-light text-[0.9rem]">
+              {service.desc}
+            </p>
+          </div>
+        ))}
       </div>
 
       <div className="container mx-auto mt-10  rounded-lg p-1">
@@ -69,7 +68,7 @@ const services = () => {
         </div>
         <ContactBaner />
       </div>
-    </>
+    </section>
   );
 };
 
