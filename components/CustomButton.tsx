3b266cc0387716a35360e324f9c3cfdf5e068054
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-03 09:20:25 +0100

    Commit: Refactor CustomButton component to directly apply Link component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -6,15 +6,15 @@ interface CustomButtonProps {
   link: string;
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({text, link}) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ text, link }) => {
   return (
-    <button className="mt-2 bg-white p-3 lg:p-5 h-13 rounded-3xl hover:text-customColor hover:bg-slate-200 transition-all duration-150">
-      <Link href={link} passHref>
+    <Link href={link} passHref>
+      <button className="mt-2 bg-white p-3 lg:p-5 h-13 rounded-3xl hover:text-customColor hover:bg-slate-200 transition-all duration-150">
         <span className="text-[0.8rem] font-sans md:text-[0.9rem] lg:text-[1.2rem] text-[#0077cc] xl:text-[16px] font-bold">
           {text}
         </span>
-      </Link>
-    </button>
+      </button>
+    </Link>
   );
 };
 
