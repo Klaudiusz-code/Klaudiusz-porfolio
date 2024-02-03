8aafe0b14e17c5cdc511e1c2b142451427639831
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-03 09:18:40 +0100

    refactor: Updated the code to directly apply the Link component to the button element.

--- components/ButtonContact.tsx
+++ components/ButtonContact.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import Link from "next/link";
 
 interface buttonProps {
-buttonText: string;
+  buttonText: string;
 }
 
-const ButtonContact: React.FC<buttonProps> = ({buttonText}) => {
+const ButtonContact: React.FC<buttonProps> = ({ buttonText }) => {
   return (
-    <button className="max-w-[200px] border border-customColor  py-2 px-5 xl:min-w-[150px] h-12 rounded-lg hover:text-customColor hover:bg-navcolor transition-all duration-150">
-      <Link href="/contact" passHref>
+    <Link href="/contact" passHref>
+      <button className="max-w-[200px] border border-customColor  py-2 px-5 xl:min-w-[150px] h-12 rounded-lg hover:text-customColor hover:bg-navcolor transition-all duration-150">
         <span className="text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] text-white xl:text-[16px] font-[400]">
           {buttonText}
         </span>
-      </Link>
-    </button>
+      </button>
+    </Link>
   );
 };
 
