8078025cf3921196759d3a38d9d047247166efe8
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-04 17:39:58 +0100

    style: Update button styling in ButtonContact component

--- components/ButtonContact.tsx
+++ components/ButtonContact.tsx
@@ -8,7 +8,7 @@ interface buttonProps {
 const ButtonContact: React.FC<buttonProps> = ({ buttonText }) => {
   return (
     <Link href="/contact" passHref>
-      <button className="max-w-[200px] border border-customColor  py-2 px-5 xl:min-w-[150px] h-12 rounded-lg hover:text-customColor hover:bg-navcolor transition-all duration-150">
+      <button className="max-w-[200px] bg-customColor py-2 px-5 xl:min-w-[150px] h-12 rounded-lg hover:bg-navcolor transition-all duration-150">
         <span className="text-[0.8rem] md:text-[0.9rem] lg:text-[1.1rem] text-white xl:text-[16px] font-[400]">
           {buttonText}
         </span>
