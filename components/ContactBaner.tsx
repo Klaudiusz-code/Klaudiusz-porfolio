6efa2a77084eef871427b4bd274dad6d41688b29
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-05 19:03:41 +0100

    style: Adjust styles and add properties to CustomButton in ContactBaner component

--- components/ContactBaner.tsx
+++ components/ContactBaner.tsx
@@ -4,7 +4,7 @@ import CustomButton from "./CustomButton";
 
 const ContactBaner = () => {
   return (
-    <div className="min-h-[400px] flex items-center justify-center bg-cover mt-32  bg-center border-spacing-6  border-[4px] border-customColor mb-2 bg-contactBanner rounded-lg">
+    <section className="container mx-auto min-h-[400px] flex items-center justify-center bg-cover mt-32  bg-center border-spacing-6 border-[1px] border-customColor mb-8 bg-contactBanner">
       <div className="text-center text-white">
         <h2 className="text-[1.4rem] md:text-[1.5rem] lg:text-[2.2rem] font-bold mb-4 font-sans">
           POROZMAWIAJMY O TWOIM{" "}
@@ -13,9 +13,9 @@ const ContactBaner = () => {
         <p className="text-[0.9rem] md:text-[1rem] lg:text-[1.1rem] mb-8">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
-        <CustomButton text="Napisz do mnie!" link="/contact" />
+        <CustomButton bgColor="#fff" textColor="#0077cc" text="Napisz do mnie!" link="/contact" />
       </div>
-    </div>
+    </section>
   );
 };
 
