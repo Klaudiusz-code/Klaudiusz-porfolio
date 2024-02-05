96a68e89a72bd0414fcf5cfccefc8c06c7bcdc8b
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-05 19:01:59 +0100

    Refactor button component to accept custom background and text colors

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,13 +4,17 @@ import React from "react";
 interface CustomButtonProps {
   text: string;
   link: string;
+  bgColor: string
+  textColor: string
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({ text, link }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ text, link, bgColor, textColor }) => {
   return (
     <Link href={link} passHref>
-      <button className="mt-2 bg-white p-3 lg:p-5 h-13 rounded-3xl hover:text-customColor hover:bg-slate-200 transition-all duration-150">
-        <span className="text-[0.8rem] font-sans md:text-[0.9rem] lg:text-[1.2rem] text-[#0077cc] xl:text-[16px] font-bold">
+      <button
+      style={{background: bgColor, color: textColor}}
+      className="mt-2 p-3 lg:p-5 h-13 rounded-3xl hover:text-customColor hover:bg-slate-200 transition-all duration-150">
+        <span className="text-[0.8rem] font-sans md:text-[0.9rem] lg:text-[1.2rem] xl:text-[16px] font-bold">
           {text}
         </span>
       </button>
