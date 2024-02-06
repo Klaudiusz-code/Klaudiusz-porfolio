a14a2e54626f1230c91d7291bc46b76bf7f4c2d9
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-06 18:34:23 +0100

    style: Updated AccordionUi component styling

--- components/Acordin/AcordinUi.tsx
+++ components/Acordin/AcordinUi.tsx
@@ -11,22 +11,28 @@ const AccordionUi: React.FC<AccordionUiProps> = ({ title, answer }) => {
   const [accordionOpen, setAccordionOpen] = useState(false);
 
   return (
-    <div className="container w-full lg:max-w-[70%] mx-auto py-3 font-sans">
+    <section className="container w-full lg:max-w-[90%] py-3 font-sans">
       <button
         onClick={() => setAccordionOpen(!accordionOpen)}
-        className="flex justify-between w-full items-center rounded-md border border-gray-700  transition-all duration-300 p-4 hover:border-customColor "
+        className="flex justify-between w-full items-center  border-b-2 border-b-gray-700  transition-all duration-300 p-4 hover:border-customColor "
       >
-        <span className="text-customColor text-[0.9rem] lg:text-[1rem]">
+        <span className="text-customColor text-[0.9rem] text-left font-thin mt-2 lg:text-[2rem]">
           {title}
         </span>
         {accordionOpen ? (
-          <HiX className="text-customColor shrink-0 ml-8 text-[1.1rem]" size={20} />
+          <HiX
+            className="text-customColor shrink-0 ml-8 text-[2rem]"
+            size={20}
+          />
         ) : (
-          <IoIosArrowDown className="text-customColor shrink-0 ml-8" size={20} />
+          <IoIosArrowDown
+            className="text-customColor shrink-0 ml-8"
+            size={20}
+          />
         )}
       </button>
       <div
-        className={`grid overflow-hidden mt-1 transition-all duration-300 ease-in-out text-gColor  text-[0.8rem] lg:text-[1rem] text-center md:text-left ${
+        className={`grid overflow-hidden mt-1 transition-all duration-300 ease-in-out text-gColor  text-[0.8rem] lg:text-[1.3rem] text-center md:text-left ${
           accordionOpen
             ? "grid-rows-[1fr] opacity-100"
             : "grid-rows-[0fr] opacity-0"
@@ -34,7 +40,7 @@ const AccordionUi: React.FC<AccordionUiProps> = ({ title, answer }) => {
       >
         <div className="overflow-hidden">{answer}</div>
       </div>
-    </div>
+    </section>
   );
 };
 
