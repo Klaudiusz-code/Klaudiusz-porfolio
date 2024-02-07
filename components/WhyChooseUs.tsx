a6ddda5123a709c4d645147ac5ca68bb99deb617
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-07 16:23:19 +0100

    style: Updated the appearance of the "Why Choose Us" section in the component.

--- components/WhyChooseUs.tsx
+++ components/WhyChooseUs.tsx
@@ -8,20 +8,19 @@ const WhyChooseUs = () => {
         Dlaczego Warto Skorzystać z Moich Usług?
       </span>
 
-      <div className="w-full border border-customColor bg-gray-800 rounded-lg  mx-auto font-sans mt-16 md:mt-20 lg:mt-24 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 items-center gap-y-4 md:gap-y-0 place-items-center place-content-center">
-        {" "}
+      <div className="mt-14 flex flex-row flex-wrap items-center justify-center gap-y-12 gap-x-16">
         {ServicesWebsite.map((service, index) => (
           <div
-            className="flex flex-col items-center justify-center text-white rounded-md max-w-[370px] min-h-[190px]"
+            className="flex items-center flex-col flex-wrap  mt-6 text-center px-2 py-6 lg:px-4 lg:py-2 max-w-[430px] min-h-[350px] lg:min-h-[420px] xl:min-h-[420px] rounded-lg bg-[#2a3245] border border-customColor"
             key={index}
           >
-            <span className="text-[2rem] md:text-[2.5rem] lg:text-[3rem] pt-1  text-customColor">
+            <span className="text-[3rem] mt-8 md:mt-10 lg:mt-14 xl:mt-16 md:text-[3.5rem] lg:text-[4rem] pt-1 text-customColor">
               {service.icon}
             </span>
-            <h1 className="mt-1 text-[1rem] md:txt-[1.1rem] lg:text-[1.3rem] font-bold font-sans">
+            <h1 className="mt-3 text-[1.7rem] md:text-[1.2rem] lg:text-[1.9rem] text-[#fff] font-bold font-sans">
               {service.title}
             </h1>
-            <p className="max-w-[80%] mt-1 text-gray-400 font-[600] text-[0.7rem] lg:text-[0.8rem]">
+            <p className="max-w-[80%] mt-2 lg:mt-5 text-gray-400 leading-7 font-[400] text-[1rem] md:text-[1.2rem] lg:text-[1.3rem]">
               {service.description}
             </p>
           </div>
