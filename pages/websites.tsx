0c37c1e361da9ce9831c9ca78674ff1d354f2889
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-12 20:26:22 +0100

    Refactor: Update UI for Websites page

--- pages/websites.tsx
+++ pages/websites.tsx
@@ -68,88 +68,88 @@ const Websites = () => {
   ];
 
   return (
-    <section className="cnt mx-auto">
-      <div>
-      <header>
-        <h1 className="text-customColor font-[500] font-sans text-[2rem] md:text-[2.5rem] xl:text-[3.7rem] ml-3 md:ml-1 lg:ml-0 mb-2 tracking-wide">
-          Strona Internetowa
-        </h1>
-      </header>
-      <motion.div
-        initial={{ opacity: 0, y: -100 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.4, delay: 0.7 }}
-        className="w-full p-8 md:p-10 lg:p-14 bg-white shadow-lg rounded-2xl shadow-[#cfcfcf] mt-6  md:mt-8 xl:mt-10 mb-6"
-      >
-        <h2 className="text-[#2f3d66] text-[1.8rem] md:text-[1.6rem] xl:text-[2.4rem]">
-          Moja Oferta dla Twojej Strony Internetowej:{" "}
-        </h2>
-        <div className="mt-8">
-          <ul className="mt-3flex flex-wrap">
-            {websiteServiceOptions.map((item, index) => (
-              <li
-                key={index}
-                className="mt-2 ml-2 text-[0.9rem] sm:text-[1.1rem] lg:text-[1.2rem] font-[400] tracking-wide text-[#0077cc] font-sans mb-3 flex items-start"
-              >
-                <span className="text-[1.3rem] text-[#0077cc] flex-shrink-0 mr-2 mt-1">
-                  <AiFillCheckSquare />
-                </span>
-                <span className="whitespace-normal">{item.title}</span>
-              </li>
-            ))}
-          </ul>
-          <div className="flex justify-center md:justify-start mt-6">
-            <CustomButton
-              text="Darmowa Wycena Projektu"
-              bgColor="#0077cc"
-              textColor="#fff"
-              link="/contact"
-            />
-          </div>
-        </div>
-      </motion.div>
-      <div className="flex flex-row items-center justify-between font-sans mt-28 p-8 md:p-10 lg:p-14 min-h-[500px] bg-white shadow-lg rounded-2xl shadow-[#cfcfcf] l group relative">
-        <div className="w-full lg:max-w-[50%] ">
+    <section>
+       <header className="bg-customColor text-center py-6">
+          <h1 className="text-white font-[500] font-sans text-[2rem] md:text-[2.5rem] xl:text-[3.7rem] ml-3 md:ml-1 lg:ml-0 mb-2 tracking-wide">
+            Strona Internetowa
+          </h1>
+        </header>
+      <div className="cnt mt-12">
+        <motion.div
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: 0.7 }}
+          className="w-full p-8 md:p-10 lg:p-14 bg-slate-100 shadow-lg rounded-2xl shadow-[#cfcfcf] mt-6  md:mt-8 xl:mt-10 mb-6"
+        >
           <h2 className="text-[#2f3d66] text-[1.8rem] md:text-[1.6rem] xl:text-[2.4rem]">
-            Korzyści z Posiadania Strony
+            Moja Oferta dla Twojej Strony Internetowej:{" "}
           </h2>
-          <ul className="mt-5 list-disc pl-4 lg:pl-6">
-            {data.map((data, index) => (
-              <li
-                key={index}
-                className="text-[0.9rem] sm:text-[1.1rem] lg:text-[1.2rem] mt-3 text-[#0077cc] tracking-wide leading-6"
-              >
-                {data.description}
-              </li>
-            ))}
-          </ul>
-          <div className="flex justify-center md:justify-start mt-6">
-            <CustomButton
-              text="Darmowa Wycena Projektu"
-              bgColor="#0077cc"
-              textColor="#fff"
-              link="/contact"
+          <div className="mt-8">
+            <ul className="mt-3flex flex-wrap">
+              {websiteServiceOptions.map((item, index) => (
+                <li
+                  key={index}
+                  className="mt-2 ml-2 text-[0.9rem] sm:text-[1.1rem] lg:text-[1.2rem] font-[400] tracking-wide text-[#0077cc] font-sans mb-3 flex items-start"
+                >
+                  <span className="text-[1.3rem] text-[#0077cc] flex-shrink-0 mr-2 mt-1">
+                    <AiFillCheckSquare />
+                  </span>
+                  <span className="whitespace-normal">{item.title}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="flex justify-center md:justify-start mt-6">
+              <CustomButton
+                text="Darmowa Wycena Projektu"
+                bgColor="#0077cc"
+                textColor="#fff"
+                link="/contact"
+              />
+            </div>
+          </div>
+        </motion.div>
+        <div className="flex flex-row items-center justify-between font-sans mt-28 p-8 md:p-10 lg:p-14 min-h-[500px] bg-slate-100  shadow-lg rounded-2xl shadow-[#cfcfcf] l group relative">
+          <div className="w-full lg:max-w-[50%] ">
+            <h2 className="text-[#2f3d66] text-[1.8rem] md:text-[1.6rem] xl:text-[2.4rem]">
+              Korzyści z Posiadania Strony
+            </h2>
+            <ul className="mt-5 list-disc pl-4 lg:pl-6">
+              {data.map((data, index) => (
+                <li
+                  key={index}
+                  className="text-[0.9rem] sm:text-[1.1rem] lg:text-[1.2rem] mt-3 text-[#0077cc] tracking-wide leading-6"
+                >
+                  {data.description}
+                </li>
+              ))}
+            </ul>
+            <div className="flex justify-center md:justify-start mt-6">
+              <CustomButton
+                text="Darmowa Wycena Projektu"
+                bgColor="#0077cc"
+                textColor="#fff"
+                link="/contact"
+              />
+            </div>
+          </div>
+
+          <div className="max-w-[50%] h-full hidden lg:block">
+            <Image
+              src={BenefitsWebsiteImg}
+              width={500}
+              height={200}
+              className="mx-auto w-full group-hover:-translate-y-4 transition-all duration-500"
+              alt="Korzysci Ze Strony Internetowej"
             />
           </div>
         </div>
-
-        <div className="absolute  right-6 max-w-[50%] h-full hidden lg:block">
-          <Image
-            src={BenefitsWebsiteImg}
-            width={500}
-            height={200}
-            className="mx-auto w-full group-hover:-translate-y-4 transition-all duration-500"
-            alt="Korzysci Ze Strony Internetowej"
-          />
+        <OfferPageTypes />
+        <div className="mt-16 md:mt-20 lg:mt-24">
+          <Acordion />
         </div>
+        <WhyChooseUs />
       </div>
-      <OfferPageTypes />
-      <div className="mt-16 md:mt-20 lg:mt-24">
-        <Acordion />
-      </div>
-      <WhyChooseUs />
       <ContactBaner />
-      </div>
     </section>
   );
 };
