650b0667ae741d50d1234a87fbf440415e07de0c
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-14 19:54:47 +0100

    Feat: Enhanced navbar with scrolling effects

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-import { MenuLinks } from "@/data";
+import { MenuLinks, subMenu } from "@/data";
 import Link from "next/link";
 import ButtonContact from "./ButtonContact";
-import { IoIosMenu } from "react-icons/io";
+import { IoIosMenu, IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -17,9 +17,33 @@ const Navbar = () => {
     setIsNavOpen(false);
   };
 
+  const [menu, setMenu] = useState(false);
+
+  useEffect(() => {
+    const scrollMenu = () => {
+      if (window.scrollY >= 80) {
+        setMenu(true);
+      } else {
+        setMenu(false);
+      }
+    };
+
+    window.addEventListener("scroll", scrollMenu);
+
+    return () => window.removeEventListener("scroll", scrollMenu);
+  }, []);
+
   return (
-    <>
-      <section className="cnt flex items-center justify-between pt-6 pb-6 relative z-10">
+    <section
+      className={`transition-all duration-500 z-50 ${
+        menu ? "fixed bg-slate-100 w-full" : "bg-transparent"
+      }`}
+    >
+      <div
+        className={`cnt flex items-center justify-between relative z-10 transition-all duration-150 ${
+          menu ? "pt-2 pb-2" : "pt-6 pb-6"
+        }`}
+      >
         <div className="flex flex-col items-center justify-center text-2xl text-white">
           <Link href="/">
             <span className="text-customColor text-[1.4rem] font-[600] font-sans">
@@ -28,7 +52,7 @@ const Navbar = () => {
           </Link>
         </div>
         <div className="flex items-center">
-          <nav className="hidden md:flex gap-y-4 gap-x-9 flex-row items-center justify-center font-sans">
+          <nav className="hidden md:flex gap-y-4 gap-x-9 flex-row items-center justify-center font-sans ">
             {MenuLinks.map((link, index) => (
               <Link
                 className={`${
@@ -38,44 +62,44 @@ const Navbar = () => {
                 key={index}
               >
                 <span className="font-[400] text-customColor">{link.name}</span>
-                <span className="ml-2 text-customColor">{link.icon}</span>
-
               </Link>
             ))}
             <ButtonContact buttonText="Darmowa Wycena" />
           </nav>
           <div className="md:hidden">
             <button
-              className="inline-flex items-center justify-center p-2 text-[1.9rem] rounded-md text-customColor focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              className="inline-flex cursor-pointer items-center justify-center p-2 text-[1.9rem] rounded-md text-customColor focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               onClick={toggleNav}
             >
               <IoIosMenu />
             </button>
-            {isNavOpen && (
-              <nav className="absolute top-full left-0 w-full  bg-white p-4">
-                {MenuLinks.map((link, index) => (
-                  <Link
-                    className={`block py-2 text-center ${
-                      link.link === currentPathname && "text-[#0077cc]"
-                    } hover:text-customColor`}
-                    href={link.link}
-                    key={index}
-                    onClick={closeNav}
-                  >
-                    <span className="text-customColor font-[400]">
-                      {link.name}
-                    </span>
-                  </Link>
-                ))}
-                <div className="w-full mt-2 mx-auto block text-center">
-                  <ButtonContact buttonText="Darmowa Wycena" />
-                </div>
-              </nav>
-            )}
+            <nav
+              className={`absolute top-full left-0 w-full bg-white p-4 transition-height duration-500 ${
+                isNavOpen ? "block" : "hidden"
+              }`}
+            >
+              {MenuLinks.map((link, index) => (
+                <Link
+                  className={`block py-2 text-center ${
+                    link.link === currentPathname && "text-[#0077cc]"
+                  } hover:text-customColor`}
+                  href={link.link}
+                  key={index}
+                  onClick={closeNav}
+                >
+                  <span className="text-customColor font-[400]">
+                    {link.name}
+                  </span>
+                </Link>
+              ))}
+              <div className="w-full mt-2 mx-auto block text-center">
+                <ButtonContact buttonText="Darmowa Wycena" />
+              </div>
+            </nav>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 };
 
