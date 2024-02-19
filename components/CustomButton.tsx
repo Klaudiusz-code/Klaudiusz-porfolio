030b2c0d2a4c5b99d0ed3f9e37376dbf4c9c6dea
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-19 17:00:28 +0100

    Refactor button corner radius

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -16,7 +16,7 @@ const CustomButton = ({
     <Link href={link} passHref>
       <button
         style={{ background: bgColor, color: textColor }}
-        className="mt-2 p-3 lg:p-4 h-13 rounded-3xl hover:text-customColor hover:scale-105 transition-all duration-200 "
+        className="mt-2 p-3 lg:p-4 h-13 rounded-2xl hover:text-customColor hover:scale-105 transition-all duration-200 "
       >
         <span className="text-[0.8rem] font-sans md:text-[0.9rem] lg:text-[1.2rem] xl:text-[16px] font-bold">
           {text}
