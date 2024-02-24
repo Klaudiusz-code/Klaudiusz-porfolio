160a521bb49d310545b938590165d561d8a814e2
Klaudiusz_dev <muminowy123@gmail.com>
2024-02-24 17:50:56 +0100

    feat: Update Navbar component with dropdown menu feature

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,54 @@
 import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
-import { MenuLinks} from "@/data";
 import Link from "next/link";
-import ButtonContact from "./ButtonContact";
 import { IoIosMenu, IoIosArrowDown } from "react-icons/io";
+import ButtonContact from "./ButtonContact";
+import { TbWorldWww } from "react-icons/tb";
+import { FaShoppingCart } from "react-icons/fa";
+
+type MenuItem = {
+  link: string;
+  name: string;
+  children?: MenuItem[];
+  icon?: React.ReactNode; 
+};
+
+type MenuItems = MenuItem[];
+
+type DropdownProps = {
+  items: MenuItems;
+};
+
+const MenuLinks: MenuItem[] = [
+  {
+    link: "/",
+    name: "Start",
+  },
+  {
+    name: "Oferta",
+    link: "/oferta",
+    children: [
+      {
+        link: "/websites",
+        name: "Strona Internetowa",
+        icon: <TbWorldWww/>
+      },
+      {
+        link: "/online-stores",
+        name: "Sklep Internetowy",
+        icon: <FaShoppingCart/>
+      },
+    ],
+  },
+  {
+    link: "/projects",
+    name: "Realizacje",
+  },
+  {
+    link: "/contact",
+    name: "Kontakt",
+  },
+];
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -33,6 +78,58 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", scrollMenu);
   }, []);
 
+  const Dropdown = ({ items }: DropdownProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => {
+      setIsOpen(!isOpen);
+    };
+
+    const openDropdown = () => {
+      setIsOpen(true);
+    };
+
+    const closeDropdown = () => {
+      setIsOpen(false);
+    };
+
+    return (
+      <div
+        onMouseEnter={openDropdown}
+        onMouseLeave={closeDropdown}
+        className={`relative`}
+      >
+        <button
+          onClick={toggleDropdown}
+          className="text-customColor flex justify-between text-lg text-center items-center font-sans"
+        >
+          Oferta
+          <span className="ml-1">
+            <IoIosArrowDown />
+          </span>
+        </button>
+        {isOpen && (
+          <div
+            className={`bg-white min-w-[250px] p-4 rounded-md transition-all duration-500 ${
+              !menu && "md:absolute"
+            }`}
+          >
+            {items.map((item, index) => (
+              <Link
+                href={item.link}
+                key={index}
+                className="hover:text-customColor flex items-center text-lg font-sans"
+              >
+                <span>{item.icon}</span>
+                <span className="block py-1 ml-2">{item.name}</span>
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  };
+
   return (
     <section
       className={`transition-all duration-500 z-50 ${
@@ -53,17 +150,23 @@ const Navbar = () => {
         </div>
         <div className="flex items-center">
           <nav className="hidden md:flex gap-y-4 gap-x-9 flex-row items-center justify-center font-sans ">
-            {MenuLinks.map((link, index) => (
-              <Link
-                className={`${
-                  link.link === currentPathname && "text-[#34C0C9]"
-                } text-[0.9rem] lg:text-[1rem] xl:text-[1.1rem] text-bgColor flex flex-row items-center hover:text-bgColor`}
-                href={link.link}
-                key={index}
-              >
-                <span className="font-[400] text-customColor">{link.name}</span>
-              </Link>
-            ))}
+            {MenuLinks.map((link, index) =>
+              link.children && link.children.length > 0 ? (
+                <Dropdown key={index} items={link.children as MenuItems} />
+              ) : (
+                <Link
+                  className={`${
+                    link.link === currentPathname && "text-[#34C0C9]"
+                  } text-[0.9rem] lg:text-[1rem] xl:text-[1.1rem] text-bgColor flex flex-row items-center hover:text-bgColor`}
+                  href={link.link}
+                  key={index}
+                >
+                  <span className="font-[400] text-customColor">
+                    {link.name}
+                  </span>
+                </Link>
+              )
+            )}
             <ButtonContact buttonText="Darmowa Wycena" />
           </nav>
           <div className="md:hidden">
@@ -78,22 +181,25 @@ const Navbar = () => {
                 isNavOpen ? "block" : "hidden"
               }`}
             >
-              {MenuLinks.map((link, index) => (
-                <Link
-                  className={`block py-2 text-center ${
-                    link.link === currentPathname && "text-[#0077cc]"
-                  } hover:text-customColor`}
-                  href={link.link}
-                  key={index}
-                  onClick={closeNav}
-                >
-                  <span className="text-customColor font-[400]">
-                    {link.name}
-                  </span>
-                </Link>
-              ))}
-              <div className="w-full mt-2 mx-auto block text-center">
-                <ButtonContact buttonText="Darmowa Wycena" />
+              {MenuLinks.map((link, index) =>
+                link.children && link.children.length > 0 ? (
+                  <Dropdown key={index} items={link.children as MenuItems} />
+                ) : (
+                  <Link
+                    className={`${
+                      link.link === currentPathname && "text-[#34C0C9]"
+                    } text-[0.9rem] lg:text-[1rem] xl:text-[1.1rem] text-lg text-bgColor flex flex-row items-center hover:text-bgColor`}
+                    href={link.link}
+                    key={index}
+                  >
+                    <span className="font-[400] text-customColor">
+                      {link.name}
+                    </span>
+                  </Link>
+                )
+              )}
+              <div className="mt-3">
+              <ButtonContact buttonText="Darmowa Wycena" />
               </div>
             </nav>
           </div>
