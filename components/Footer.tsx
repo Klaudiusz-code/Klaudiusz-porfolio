b7155e69b7ef2bc3b1e95d8ccb9143a71ef018b6
Klaudiusz_dev <muminowy123@gmail.com>
2024-03-10 16:58:36 +0100

    Ref: Refactor Footer component and add new features

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { IoMdMail } from "react-icons/io";
 import { FaPhone } from "react-icons/fa";
+import Link from "next/link";
+import CustomButton from "./CustomButton";
 
 const ContactIcon = [
   {
@@ -33,47 +35,41 @@ const MenuLinks = [
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-[#0a1b20] py-4 font-sans">
-      <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center text-center lg:text-left p-2">
-        <div className="flex flex-col lg:w-1/3">
-          <h3 className="text-3xl lg:text-4xl text-gray-300 font-semibold mb-2">
-            Potrzebujesz pomocy w projekcie?
-          </h3>
-          <p className="text-gray-200 tracking-wide text-md mb-4 mt-2">
-            Jesteś w dobrym miejscu! Jako freelancer zajmuję się tworzeniem
-            responsywnych stron internetowych oraz sklepów online.
-          </p>
-
-          <div className="flex justify-center lg:justify-start lg:w-1/3 space-x-4 mb-4 lg:mb-0">
-            {ContactIcon.map((item, index) => (
-              <a
-                key={index}
-                href={item.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-white hover:text-gray-300 text-2xl transition duration-300"
-              >
-                {item.icon}
-              </a>
-            ))}
+    <footer className="w-full bg-[#0c0c0c] py-8">
+      <div className="cnt mx-auto flex flex-col py-8  justify-between items-center text-center lg:text-left px-2">
+        <h3 className="text-2xl lg:text-3xl text-gray-200 tracking-wide font-bold font-archivo mb-2 text-center">
+          Masz Pytanie? Z chęcią na nie odpowiem!
+        </h3>
+        <div className="w-full flex flex-col gap-y-12 lg:flex-row justify-between mt-12 font-sans">
+          <div>
+            <Link
+              href="/"
+              className="relative lg:text-2xl text-gray-300 tracking-wide text-md mb-4 mt-2 lg:w-3/4 group"
+            >
+              adamaszek404@gmail.com
+              <div className="h-[0.04rem] bg-slate-400 w-full absolute -bottom-4 right-0 group-hover:bg-blue-400 transition-all duration-300"></div>
+            </Link>
+          </div>
+          <div>
+            <CustomButton
+              bgColor="#363636"
+              textColor="#fff"
+              text="Napisz do mnie!"
+              link="/contact"
+            />
+          </div>
+          <div>
+            <Link
+              href="/"
+              className="relative lg:text-2xl text-gray-300 tracking-wide text-md mb-4 mt-2 lg:w-3/4 group"
+            >
+              +48 519 668 439
+              <div className="h-[0.04rem] bg-slate-400 w-full absolute -bottom-4 right-0 group-hover:bg-blue-400 transition-all duration-300"></div>
+            </Link>
           </div>
         </div>
-        <nav className="lg:w-1/3">
-          <ul className="flex flex-col justify-center gap-y-4 items-center">
-            {MenuLinks.map((item, index) => (
-              <li key={index}>
-                <a
-                  href={item.link}
-                  className="text-gray-300 underline  hover:text-gray-300 transition text-xl duration-300"
-                >
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
       </div>
-      <p className="text-white text-lg mt-4 text-center">
+      <p className="text-gray-400 text-lg mt-4 text-center">
         © Klaudiusz Adamaszek | Web Development 2024
       </p>
     </footer>
