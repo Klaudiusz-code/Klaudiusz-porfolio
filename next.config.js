18a767a7faf23a53cd6c77f888aa88543bf99d11
necodeus <dawid@necodeo.com>
2024-06-13 18:26:18 +0200

    feat: change graphql url

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'adamaszekwebdev.pl',
+        protocol: 'http',
+        hostname: 'klaudiuszdev.pl',
         port: '',
         pathname: '/wp-content/**',
       },
