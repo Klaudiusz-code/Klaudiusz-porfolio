f94766ef57b3d2e4ec95036dc9ef99143ed12573
Klaudiusz_dev <muminowy123@gmail.com>
2024-06-18 18:15:04 +0200

    update

--- components/OfferPageTypes.tsx
+++ components/OfferPageTypes.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import CustomButton from "./CustomButton";
+import { IconType } from "react-icons";
 import { FaFileAlt, FaBuilding, FaBlog } from "react-icons/fa";
 
-const OfferPageTypes = ({ data }: any) => {
-  const icons = [<FaFileAlt />, <FaBuilding />, <FaBlog />];
+const iconComponents: { [key: string]: IconType } = {
+  FaFileAlt,
+  FaBuilding,
+  FaBlog,
+};
 
+const OfferPageTypes = ({ data }: any) => {
   return (
     <section className="container text-center mx-auto mt-28">
       <h3 className="bg-customColor px-4 py-2 rounded-lg text-white text-[1.2rem] lg:text-[1.5rem] inline-block font-sans mb-8">
@@ -14,36 +19,40 @@ const OfferPageTypes = ({ data }: any) => {
         {data.description}
       </p>
       <div className="mt-20 flex flex-row flex-wrap justify-center gap-y-16 gap-x-16">
-        {data.items.map((item: any, index: any) => (
-          <div
-            key={index}
-            className="relative flex flex-col items-center text-center text-customColor p-8 min-h-[300px] max-w-[350px] bg-white rounded-2xl shadow-lg transition-transform duration-300 transform hover:translate-y-[-10px] hover:scale-105 hover:shadow-2xl group"
-          >
-            <div className="absolute top-0 right-0 bg-customColor text-white font-bold text-4xl p-4 rounded-bl-2xl shadow-lg">
-              1
-            </div>
-            <div className="relative mt-12 mb-6">
-              <div className="bg-customColor rounded-full h-16 w-16 flex items-center justify-center text-white text-4xl mb-6">
-                {icons[index]}
-              </div>
-              <h2 className="text-gray-800 font-sans text-xl lg:text-2xl font-semibold mb-4">
-                {item.title}
-              </h2>
-              <p className="text-gray-600 text-sm lg:text-base xl:text-lg font-mono mt-2 mb-6 leading-8">
-                {item.description}
-              </p>
+        {data.items.map((item: any, index: number) => {
+            console.log(item.icon); 
+          const IconComponent = iconComponents[item.icon];
 
-              <div className="mt-auto">
-                <CustomButton
-                  bgColor="#0077cc"
-                  textColor="#fff"
-                  text="Darmowa Wycena Projektu"
-                  link="/contact"
-                />
+          return (
+            <div
+              key={index}
+              className="relative flex flex-col items-center text-center text-customColor p-8 min-h-[300px] max-w-[350px] bg-white rounded-2xl shadow-lg transition-transform duration-300 transform hover:translate-y-[-10px] hover:scale-105 hover:shadow-2xl group"
+            >
+              <div className="absolute top-0 right-0 bg-customColor text-white font-bold text-4xl p-4 rounded-bl-2xl shadow-lg">
+                {index + 1}
+              </div>
+              <div className="relative mt-12 mb-6">
+                <div className="bg-customColor rounded-full h-16 w-16 flex items-center justify-center text-white text-4xl mb-6">
+                  {IconComponent && <IconComponent />}
+                </div>
+                <h2 className="text-gray-800 font-sans text-xl lg:text-2xl font-semibold mb-4">
+                  {item.title}
+                </h2>
+                <p className="text-gray-600 text-sm lg:text-base xl:text-lg font-mono mt-2 mb-6 leading-8">
+                  {item.description}
+                </p>
+                <div className="mt-auto">
+                  <CustomButton
+                    bgColor="#0077cc"
+                    textColor="#fff"
+                    text="Darmowa Wycena Projektu"
+                    link="/contact"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </section>
   );
