83823ceffd8b9416930b785ea7b3b9a96c656bb4
Klaudiusz_dev <muminowy123@gmail.com>
2024-11-23 12:30:58 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,39 @@
-import './globals.css';
-import Head from 'next/head';
+import "./globals.css";
+import Head from "next/head";
 
-import { query } from '@/ApolloClient';
+import { query } from "@/ApolloClient";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 import GRAPHQL_QUERY from "@/gql-queries/menus.graphql";
-import { MenusQuery, MenusQueryVariables } from '@/gql/graphql';
+import { MenusQuery, MenusQueryVariables } from "@/gql/graphql";
 
 export default async function RootLayout({ children }: any) {
-  const { data } = await query<MenusQuery, MenusQueryVariables>({ query: GRAPHQL_QUERY });
+  const { data } = await query<MenusQuery, MenusQueryVariables>({
+    query: GRAPHQL_QUERY,
+  });
 
-  const header = data.menus?.nodes.find((node: any) => node.slug === 'header');
-  const footer = data.menus?.nodes.find((node: any) => node.slug === 'footer');
+  const header = data.menus?.nodes.find((node: any) => node.slug === "header");
+  const footer = data.menus?.nodes.find((node: any) => node.slug === "footer");
 
   return (
     <html lang="pl">
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-94LB4ZY02J"></script>
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-94LB4ZY02J"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-94LB4ZY02J');
-            `,
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-94LB4ZY02J');
+          `,
           }}
         />
-      </Head>
+      </head>
       <body>
         <Navbar data={header} />
         {children}
